1d83021760c2b92332a644498aadd3579584b863
Zhiwu Mao <zwmao@outlook.com>
2021-08-23 15:24:44 +0800

    web版，完善保存配置功能

--- web/run.py
+++ web/run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask import render_template
 import os,json
 app=Flask(__name__)
@@ -9,8 +9,19 @@ def index(name=None):
     print(config)
     return render_template('index.html',config=config)
 
+@app.route('/saveConfig',methods=['POST'])
+def saveConfig():
+    config=json.loads(request.form["config"])
+    filename = getConfigFileUrl()
+    with open(filename,'w') as blog_file:
+        blog_file.write(json.dumps(config))
+    return "ok"
+
+def getConfigFileUrl():
+    return os.path.join(app.static_folder,  os.path.abspath(os.path.dirname(__file__))+'/config.json')
+
 def getConfig():
-    filename = os.path.join(app.static_folder,  os.path.abspath(os.path.dirname(__file__))+'/config.json')
+    filename = getConfigFileUrl()
     with open(filename) as blog_file:
         data = json.load(blog_file)
     return data
