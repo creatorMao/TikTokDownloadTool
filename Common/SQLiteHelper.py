1bece3a73ff1cdd5933a2d2b8f6acc72a557c67a
Zhiwu Mao <zwmao@outlook.com>
2021-09-04 23:58:38 +0800

    初始化SQLite帮助类

--- Common/SQLiteHelper.py
+++ Common/SQLiteHelper.py
@@ -6,10 +6,31 @@ class SQLiteHelper:
         self.db=sqlite3.connect(dbFilePath)
         self.cursor=self.db.cursor()
         pass
-    
-    def execute(self,sql):
-        self.cursor.execute(sql)
 
-        if self.db.total_changes>0:
+    def close(self):
+        self.db.close()
+    
+    def execute(self,sql,param=None):
+        if param is None:
+            self.cursor.execute(sql)
+        else:
+            if type(param) is list:
+                self.cursor.executemany(sql,param)
+            else:
+                self.cursor.execute(sql,param)
+
+        count=self.db.total_changes
+
+        self.db.commit()
+
+        if count>0:
             return True
         return False
+    
+    def query(self,sql,param=None):
+        if param is None:
+            self.cursor.execute(sql)
+        else:
+            self.cursor.execute(sql,param)
+        
+        return self.cursor.fetchall()
