7e744669f31c8ec004edbce678ac00da74f74889
Zhiwu Mao <zwmao@outlook.com>
2021-09-04 23:01:35 +0800

    初始化SQLite帮助类

---
+++ Common/SQLiteHelper.py
@@ -0,0 +1,16 @@
+from re import S
+import sqlite3
+
+class SQLiteHelper:
+
+    def __init__(self,dbFilePath):
+        self.db=sqlite3.connect(dbFilePath)
+        self.cursor=self.db.cursor()
+        pass
+    
+    def execute(self,sql):
+        self.cursor.execute(sql)
+        
+        if self.db.total_changes>0:
+            return True
+        return False
