50ea72457cdde2537dd59624fb4487badd568c84
Zhiwu Mao <zwmao@outlook.com>
2022-01-05 19:56:00 +0800

    默认不开启db

--- TikTokMulti.py
+++ TikTokMulti.py
@@ -15,7 +15,7 @@ class TikTok():
             'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
         }
         
-        self.dbService=DBService.DBService()
+        #self.dbService=DBService.DBService()
 
         #绘制布局
         print("#" * 130)
